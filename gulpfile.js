6f927c3a896c0a32bc9ebaeed6712bc68e50795c
Виталий Жилкин <vetll25@icloud.com>
2020-03-02 13:31:08 +0500

    add cssfile and gulpmincss

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
 
 gulp.task("hello", function(done) {
   console.log("Привет Мир");
@@ -13,4 +15,15 @@ gulp.task("browser-sync", function() {
     }
   });
   gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+gulp.task("mincss", function() {
+  return gulp
+    .src("src/css/*.css")
+
+    .pipe(rename({ suffix: ".min" }))
+
+    .pipe(cleanCSS())
+
+    .pipe(gulp.dest("app/css"));
 });
