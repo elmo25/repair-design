e099ac71a379158e1f0d7d25ee4362b81cab9c9c
Виталий Жилкин <vetll25@icloud.com>
2020-03-05 15:24:16 +0500

    feat: added futura book & demi fonts

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const browserSync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 function bs() {
   serveSass();
@@ -30,6 +31,11 @@ function minCSS() {
 function serveSass() {
   return src("./sass/*.sass")
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 4 versions"]
+      })
+    )
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
 }
