258d57385214548beb9cc46ed847795aab47d2bf
Виталий Жилкин <vetll25@icloud.com>
2020-03-12 19:15:55 +0500

    feat: jquery library and button up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector(".modal");
   const modalBtn = document.querySelectorAll("[data-toggle=modal]");
@@ -8,10 +9,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   modalBtn.forEach(element => {
     element.addEventListener("click", switchModal);
   });
-
   closeBtn.addEventListener("click", switchModal);
   //закрыть по крестику
-
   document.addEventListener("keyup", e => {
     const key = e.keyCode;
     if (key == 27) {
@@ -25,4 +24,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
   });
   //закрыть по клику вне окна
+});
+*/
+$(document).ready(function() {
+  var modal = $(".modal"),
+    modalBtn = $("[data-toggle=modal]"),
+    closeBtn = $(".modal__close"),
+    button = $(".go-top");
+
+  modalBtn.on("click", function() {
+    modal.toggleClass("modal--visible");
+  });
+  closeBtn.on("click", function() {
+    modal.toggleClass("modal--visible");
+  });
+  $(window).on("scroll", e => {
+    if ($(this).scrollTop() >= 50) {
+      button.fadeIn();
+    } else {
+      button.fadeOut();
+    }
+  });
+  button.on("click", e => {
+    e.preventDefault();
+    $("html").animate({ scrollTop: 0 }, 1000);
+  });
 });
