b89853137d78aa805ad6dee41f0bc719ed9f643f
Виталий Жилкин <vetll25@icloud.com>
2020-03-15 22:25:25 +0500

    feat:added section steps and slider

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var modal = $(".modal"),
     modalBtn = $("[data-toggle=modal]"),
     closeBtn = $(".modal__close"),
-    container = $(".contaienr"),
+    modalDialog = $(".modal__dialog"),
     button = $(".go-top");
 
   modalBtn.on("click", function(e) {
@@ -11,7 +11,6 @@ $(document).ready(function() {
   closeBtn.on("click", function() {
     modal.toggleClass("modal--visible");
   });
-
   $(window).on("scroll", e => {
     if ($(this).scrollTop() >= 50) {
       button.fadeIn();
@@ -24,6 +23,22 @@ $(document).ready(function() {
     $("html").animate({ scrollTop: 0 }, 1000);
   });
   //кнопка на вверх
+  $(document).keyup(function(event) {
+    if (event.which == "27") {
+      $(".modal").removeClass("modal--visible");
+    }
+  });
+  //закрыть на esc
+  $(document).on("click", function(event) {
+    if (event.target == ".modal") {
+      modal.removeClass("modal--visible");
+    }
+  });
+  modal.on("click", function(e) {
+    if (!modalDialog.is(e.target) && modalDialog.has(e.target).length === 0) {
+      modal.toggleClass("modal--visible");
+    }
+  });
   var mySwiper = new Swiper(".swiper-container", {
     // Optional parameters
     loop: true,
@@ -42,4 +57,41 @@ $(document).ready(function() {
 
   next.css("left", prev.width() + 10 + bullets.width() + 10);
   bullets.css("left", prev.width() + 10);
+
+  var steps = new Swiper(".steps__swiper-container", {
+    // Optional parameters
+    loop: true,
+
+    pagination: {
+      el: ".steps__swiper-pagination",
+      type: "bullets"
+    },
+    navigation: {
+      nextEl: ".steps__swiper-button-next",
+      prevEl: ".steps__swiper-button-prev"
+    }
+  });
+  var next = $(".steps__swiper-button-next");
+  var prev = $(".steps__swiper-button-prev");
+  var bullets = $(".steps__swiper-pagination");
+
+  next.css("left", prev.width() + 10 + bullets.width() + 10);
+  bullets.css("left", prev.width() + 10);
+
+  $(".left-item").on("click", function() {
+    $(".left-item").removeClass("--active");
+    $(this).addClass("--active");
+    const e = $(this).data("index");
+    steps.slideTo(e);
+  });
+  steps.on("slideChange", function() {
+    let e = steps.activeIndex - 0;
+    if (e === 5) {
+      e = 0;
+    }
+    $(".left-item").removeClass("--active");
+    $(".left-item")
+      .eq(e)
+      .addClass("--active");
+  });
 });
